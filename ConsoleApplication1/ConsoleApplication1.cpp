85883e8fdaf7f2c8ec5dd8af20b9c9a72d829d92
Rita Gorokhod <rita.gorokhod@sonarsource.com>
2020-03-20 12:11:22 +0100

    fix build

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,15 +5,4 @@ int* f1(void) {
     return &local_auto; // 946 Noncompliant, returning address of an object allocated on the stack.
 }
 
-using namespace std;
-
-class S {
-public:
-    explicit S(int a, int b);
-};
-
-void g(shared_ptr<S> p1, shared_ptr<S> p2);
-
-void f() {
-    g(shared_ptr<S>(new S(1, 2)), shared_ptr<S>(new S(3, 4))); // 5502 Noncompliant: 2 resources are allocated in the same expression statement
-}
+static const char str[] = "(Date should be in the form ??-??-??)"; // Noncompliant. Evaluates to "(Date should be in the form ~~]"
